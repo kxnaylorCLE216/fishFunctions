4eae8f9b3f66925187788e64d77635db9085d8e1
kxnaylorcle216 <terminal109@protonmail.com>
2021-08-24 20:29:00 -0400

    fixed cat, added dadjoke

--- cat.fish
+++ cat.fish
@@ -1,1 +1,10 @@
-function cat; bat; end
+function cat -d "Use bat instead of cat unless it's a Markdown file, then use mdless"
+	set -l exts md markdown txt
+
+	if not test -f $argv
+		echo "File not found: $argv"
+		return 0
+	end
+
+	command bat --style plain --theme OneHalfDark $argv
+end
